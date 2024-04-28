2241195c3e07aa84da6e71c169dd6ea317fd839a
iooosef <josence22@gmail.com>
2024-04-28 16:52:41 +0800

    Changed dir of Main class

--- src/main/java/ph/edu/tip/mamamoo/Main.java
+++ src/main/java/ph/edu/tip/mamamoo/Main.java
@@ -1,7 +1,7 @@
-package ph.edu.tip;
+package ph.edu.tip.mamamoo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worlds!");
     }
 }
