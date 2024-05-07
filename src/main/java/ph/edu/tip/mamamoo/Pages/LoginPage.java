91248874928733536502fbe62d1e9e731d3394a9
iooosef <josence22@gmail.com>
2024-05-07 09:15:48 +0800

    Added LoginPage class

---
+++ src/main/java/ph/edu/tip/mamamoo/Pages/LoginPage.java
@@ -0,0 +1,49 @@
+package ph.edu.tip.mamamoo.Pages;
+
+import ph.edu.tip.mamamoo.ActionListeners.LoginListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class LoginPage extends JPanel {
+    private JTextField usernameBox;
+    private JPasswordField passwordBox;
+    private JButton loginBtn;
+    private JLabel statusLbl;
+    private LoginListener loginListener;
+
+    public LoginPage() {
+        usernameBox = new JTextField(20);
+        passwordBox = new JPasswordField(16);
+        loginBtn = new JButton("Login");
+        statusLbl = new JLabel();
+
+        add(new Label("Username:"));
+        add(usernameBox);
+        add(new Label("Password:"));
+        add(passwordBox);
+        add(loginBtn);
+        add(statusLbl);
+    }
+    public String getUsername() {
+        return usernameBox.getText();
+    }
+    public char[] getPassword() {
+        return passwordBox.getPassword();
+    }
+    public void setStatus(String status) {
+        statusLbl.setText(status);
+    }
+    public void addLoginListener(LoginListener listener) {
+        this.loginListener = listener;
+        loginBtn.addActionListener(listener);
+    }
+    public void showLoginPage() {
+        setVisible(true);
+    }
+    public void hideLoginPage() {
+        setVisible(false);
+    }
+
+}
