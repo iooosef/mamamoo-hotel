5b8e917d09954bedfd1d5d2c3a5b1dce832fc7ca
iooosef <josence22@gmail.com>
2024-05-07 10:27:54 +0800

    Added RegisterPage.java

---
+++ src/main/java/ph/edu/tip/mamamoo/Pages/RegisterPage.java
@@ -0,0 +1,12 @@
+package ph.edu.tip.mamamoo.Pages;
+
+import javax.swing.*;
+
+public class RegisterPage extends JPanel {
+    private JLabel label;
+    public RegisterPage(){
+        label = new JLabel("Registration Page HERE");
+        add(label);
+    }
+
+}
