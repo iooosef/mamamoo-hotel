5c4cdaae7fe48533783d36d843be124d2efa6ff7
iooosef <josence22@gmail.com>
2024-05-07 10:29:30 +0800

    Added GotToRegisterListener and Register Page to MultiPageApp.java

--- src/main/java/ph/edu/tip/mamamoo/MultiPageApp.java
+++ src/main/java/ph/edu/tip/mamamoo/MultiPageApp.java
@@ -1,8 +1,10 @@
 package ph.edu.tip.mamamoo;
 
+import ph.edu.tip.mamamoo.ActionListeners.GoToRegisterListener;
 import ph.edu.tip.mamamoo.ActionListeners.LoginListener;
 import ph.edu.tip.mamamoo.Pages.HomePage;
 import ph.edu.tip.mamamoo.Pages.LoginPage;
+import ph.edu.tip.mamamoo.Pages.RegisterPage;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,6 +12,7 @@ import java.awt.*;
 public class MultiPageApp extends JFrame {
     private LoginPage loginPage;
     private HomePage homePage;
+    private RegisterPage registerPage;
     public MultiPageApp(){
         setTitle("Mamamoo Hotel");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,10 +21,13 @@ public class MultiPageApp extends JFrame {
 
         loginPage = new LoginPage();
         homePage = new HomePage();
+        registerPage = new RegisterPage();
 
         LoginListener loginListener = new LoginListener(this, loginPage, homePage);
+        GoToRegisterListener goToRegisterListener = new GoToRegisterListener(this, loginPage, registerPage);
 
         loginPage.addLoginListener(loginListener);
+        loginPage.addGoToRegisterListener(goToRegisterListener);
         showLoginPage();
     }
 
