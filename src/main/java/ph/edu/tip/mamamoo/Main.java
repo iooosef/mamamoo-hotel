9bab57331b7b46413145715ccf9a5f46caad765c
iooosef <josence22@gmail.com>
2024-05-07 09:26:41 +0800

    Changed Main.java

--- src/main/java/ph/edu/tip/mamamoo/Main.java
+++ src/main/java/ph/edu/tip/mamamoo/Main.java
@@ -1,7 +1,19 @@
 package ph.edu.tip.mamamoo;
 
+import ph.edu.tip.mamamoo.Data.SqlServerInit;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlds!");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MultiPageApp().setVisible(true);
+            }
+        });
     }
 }
