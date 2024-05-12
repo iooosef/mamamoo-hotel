566b1402bbea6c4a15fcfda7c55b6a09f00d7a8d
iooosef <josence22@gmail.com>
2024-05-12 17:22:42 +0800

    Added RoomServicePage.java

---
+++ src/main/java/ph/edu/tip/mamamoo/Pages/RoomServicePage.java
@@ -0,0 +1,31 @@
+package ph.edu.tip.mamamoo.Pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ph.edu.tip.mamamoo.ActionListeners.NavButtonListener;
+import ph.edu.tip.mamamoo.MultiPageApp;
+import ph.edu.tip.mamamoo.Pages.Shared.MainPanel;
+import ph.edu.tip.mamamoo.Pages.Shared.NavBar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoomServicePage extends JPanel {
+    final Logger _logger = LogManager.getLogger();
+    private MultiPageApp app;
+    private NavBar navBar;
+    private MainPanel mainPanel;
+    public RoomServicePage(MultiPageApp app){
+        this.app = app;
+        this.setLayout(new BorderLayout());
+        navBar = new NavBar(app, this);
+        this.add(navBar, BorderLayout.WEST);
+
+        this.mainPanel = new MainPanel("Room Service");
+        this.add(mainPanel, BorderLayout.CENTER);
+    }
+
+    public NavBar getNavBar() {
+        return navBar;
+    }
+}
