b3b0e9b1da131787671798f74487d868c5e65c4e
iooosef <josence22@gmail.com>
2024-05-12 17:31:21 +0800

    Changed NavButtons to NavButtonsModel and added getNavButtons()

--- src/main/java/ph/edu/tip/mamamoo/Pages/Shared/NavBar.java
+++ src/main/java/ph/edu/tip/mamamoo/Pages/Shared/NavBar.java
@@ -16,7 +16,12 @@ import java.io.File;
 public class NavBar extends JPanel {
     final Logger _logger = LogManager.getLogger();
     private JLabel logoLabel;
-    public NavBar() {
+    private NavButtonsModel navButtons;
+
+    public NavBar(MultiPageApp app, JPanel parentPanel) {
+        this.app = app;
+        this.parentPanel = parentPanel;
+
         this.setLayout(new MigLayout("flowy, gapy 10, insets 10, align center"));
         this.setPreferredSize(new Dimension(200, 768));
         this.setBackground(new Color(0, 120, 215));
@@ -24,18 +29,18 @@ public class NavBar extends JPanel {
         logoLabel = getLogoLabel(logoLabel,"./static/images/logo_sidebar.png", 175, -1);
         this.add(logoLabel);
 
-        NavButton homeNavBtn = new NavButton(" Home", IconUtility.getIcon("./static/images/home_ico.png", 24, 24));
-        homeNavBtn.setDefaultBgColor(new Color(4, 58, 78));
-        this.add(homeNavBtn, "growx");
+        this.navButtons = new NavButtonsModel();
+        this.navButtons.homeButton = new NavButton(" Home", IconUtility.getIcon("./static/images/home_ico.png", 24, 24));
+        this.add(this.navButtons.homeButton, "growx");
 
-        NavButton roomsNavBtn = new NavButton(" Rooms", IconUtility.getIcon("./static/images/rooms_ico.png", 24, 24));
-        this.add(roomsNavBtn, "growx");
+        this.navButtons.roomsButton = new NavButton(" Rooms", IconUtility.getIcon("./static/images/rooms_ico.png", 24, 24));
+        this.add(this.navButtons.roomsButton, "growx");
 
-        NavButton bookingsNavBtn = new NavButton(" Bookings", IconUtility.getIcon("./static/images/bookings_ico.png", 24, 24));
-        this.add(bookingsNavBtn, "growx");
+        this.navButtons.bookingsButton = new NavButton(" Bookings", IconUtility.getIcon("./static/images/bookings_ico.png", 24, 24));
+        this.add(this.navButtons.bookingsButton, "growx");
 
-        NavButton roomServiceNavBtn = new NavButton(" Room Service", IconUtility.getIcon("./static/images/room_service_ico.png", 24, 24));
-        this.add(roomServiceNavBtn, "growx");
+        this.navButtons.roomServiceButton = new NavButton(" Room Service", IconUtility.getIcon("./static/images/room_service_ico.png", 24, 24));
+        this.add(this.navButtons.roomServiceButton, "growx");
 
         ClockPanel clock = new ClockPanel();
         JPanel alignYEnd = new JPanel();
@@ -60,5 +65,9 @@ public class NavBar extends JPanel {
 
     public void addNavButtonListenerTo(NavButtonListener listener, NavButton selectedBtn) {
         selectedBtn.addActionListener(listener);
+    }
+
+    public NavButtonsModel getNavButtons() {
+        return navButtons;
     }
 }
