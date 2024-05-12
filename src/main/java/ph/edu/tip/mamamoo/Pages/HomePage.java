2b504741da7560442d6cf95550566cd18ccc3da0
iooosef <josence22@gmail.com>
2024-05-12 15:06:45 +0800

    Added MainPanel for HomePage.java

--- src/main/java/ph/edu/tip/mamamoo/Pages/HomePage.java
+++ src/main/java/ph/edu/tip/mamamoo/Pages/HomePage.java
@@ -3,6 +3,7 @@ package ph.edu.tip.mamamoo.Pages;
 import net.miginfocom.swing.MigLayout;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ph.edu.tip.mamamoo.Pages.Shared.MainPanel;
 import ph.edu.tip.mamamoo.Pages.Shared.NavBar;
 
 import javax.swing.*;
@@ -11,9 +12,13 @@ import java.awt.*;
 public class HomePage extends JPanel {
     final Logger _logger = LogManager.getLogger();
     private NavBar navBar;
+    private MainPanel mainPanel;
     public HomePage(){
         this.setLayout(new BorderLayout());
         navBar = new NavBar();
         this.add(navBar, BorderLayout.WEST);
+
+        this.mainPanel = new MainPanel("Booked-in Rooms Dashboard");
+        this.add(mainPanel, BorderLayout.CENTER);
     }
 }
