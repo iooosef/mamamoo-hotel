8376870ba45a669e3cd60bfe9933549ff11a81e1
iooosef <josence22@gmail.com>
2024-05-12 14:47:00 +0800

    Added color params for OnNavButtonPressListener.java

--- src/main/java/ph/edu/tip/mamamoo/ActionListeners/OnNavButtonPressListener.java
+++ src/main/java/ph/edu/tip/mamamoo/ActionListeners/OnNavButtonPressListener.java
@@ -6,16 +6,23 @@ import java.awt.*;
 
 public class OnNavButtonPressListener implements ChangeListener {
     private JButton button;
+    public Color bgColor = new Color(0, 120, 215);
+    public Color bgColorPressed = new Color(10, 88, 202);
     public OnNavButtonPressListener(JButton button){
         this.button = button;
+    }
+    public OnNavButtonPressListener(JButton button, Color bgColor, Color bgColorPressed) {
+        this(button);
+        this.bgColor = bgColor;
+        this.bgColorPressed = bgColorPressed;
     }
     @Override
     public void stateChanged(javax.swing.event.ChangeEvent e) {
         ButtonModel model = button.getModel();
         if (model.isPressed() && model.isArmed()) {
-            button.setBackground(new Color(10, 88, 202));
+            button.setBackground(bgColorPressed);
         } else {
-            button.setBackground(new Color(0, 120, 215));
+            button.setBackground(bgColor);
         }
     }
 }
