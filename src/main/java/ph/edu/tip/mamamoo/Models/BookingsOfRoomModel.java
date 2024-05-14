a3cb248e41eff14eac7b0ebc223355f16185e8a8
iooosef <josence22@gmail.com>
2024-05-15 00:13:05 +0800

    Added BookingsOfRoomModel.java

---
+++ src/main/java/ph/edu/tip/mamamoo/Models/BookingsOfRoomModel.java
@@ -0,0 +1,10 @@
+package ph.edu.tip.mamamoo.Models;
+
+import java.sql.Timestamp;
+
+public class BookingsOfRoomModel {
+    public int id;
+    public Timestamp checkIn;
+    public Timestamp checkOut;
+    public String status;
+}
