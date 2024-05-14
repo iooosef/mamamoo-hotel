401e96de51416d54ed938e23d8eda8f339d97a60
iooosef <josence22@gmail.com>
2024-05-15 00:16:09 +0800

    Added bkng_fee to RoomTypes datainit

--- docs/datainit_RoomTypes.sql
+++ docs/datainit_RoomTypes.sql
@@ -1,9 +1,9 @@
 USE HotelAppDB
 GO
 INSERT INTO dbo.RoomTypes
-(room_type_name)
+(room_type_name,bkng_fee)
 VALUES
-('Standard'),
-('Premium'),
-('One-bedroom Suite'),
-('Two-bedroom Suite')
+('Standard', 200),
+('Premium', 400),
+('Suite A', 900),
+('Suite B', 1200)
