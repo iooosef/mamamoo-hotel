eb7baaaedbdfcbf18a5b2d93b57de60d54df9a89
iooosef <josence22@gmail.com>
2024-05-15 02:31:16 +0800

    Added code to show AccountControlsDialog

--- src/main/java/ph/edu/tip/mamamoo/Pages/Shared/MainPanel.java
+++ src/main/java/ph/edu/tip/mamamoo/Pages/Shared/MainPanel.java
@@ -4,16 +4,22 @@ import net.miginfocom.swing.MigLayout;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ph.edu.tip.mamamoo.Components.NavButton;
+import ph.edu.tip.mamamoo.Dialogs.AccountControlsDialog;
+import ph.edu.tip.mamamoo.MultiPageApp;
 import ph.edu.tip.mamamoo.Utilities.IconUtility;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPanel extends JPanel {
     final Logger _logger = LogManager.getLogger();
-    JPanel headerPanel;
-    JLabel headerLabel;
     private MultiPageApp app;
+    private JPanel headerPanel;
+    private JLabel headerLabel;
     NavButton acctButton;
     public MainPanel(String headerText, MultiPageApp app) {
         this.setLayout(new MigLayout("flowy, inset 0, gap 0 0"));
@@ -37,5 +43,13 @@ public class MainPanel extends JPanel {
         this.acctButton.setMargin(new Insets(5,5,5,5));
         this.acctButton.setFont(new Font("Sans Serif", Font.PLAIN, 18));
         this.headerPanel.add(acctButton, "gap 5 5 5 5");
+
+        this.acctButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AccountControlsDialog dialog = new AccountControlsDialog(app, app);
+                dialog.setVisible(true);
+            }
+        });
     }
 }
